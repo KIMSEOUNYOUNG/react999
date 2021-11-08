2260272ab872819d85ddb6eddb1c3a983b9cd675
heoar <87642320+junaky123@users.noreply.github.com>
2021-11-08 15:49:07 +0900

    2021.11.08

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,6 +3,16 @@ import Header from "../layouts/Header";
 import Footer from "../layouts/Footer";
 import Contents from "../layouts/Contents";
 
+function Info({text}){
+    return <div>{text}</div>
+}
+
+const textInfo = [
+    {text : "you are"},
+    {text : "already"},
+    {text : "doing well."},
+]
+
 function Contact(){
     return (
         <div>
@@ -10,10 +20,10 @@ function Contact(){
             <Contents>
                 <section id="mainCont">
                     <h2 className="sr-only">연락처 컨텐츠 영역입니다.</h2>
-                    <div className="main__cont">
-                        <div className="l1">You are</div>
-                        <div className="l2">already </div>
-                        <div className="l3">doing well.</div>
+                    <div className="main__cont tact">
+                        {textInfo.map((info) => (
+                            <Info text = {info.text} key={info.text} />
+                        ))}
                     </div>
                 </section>
             </Contents>
