d29d99fabd8c9bcb8e1c5d0b01e583dbcc472485
heoar <87642320+junaky123@users.noreply.github.com>
2021-11-15 14:31:03 +0900

    2021.11.15

--- src/components/pages/Youtube.js
+++ src/components/pages/Youtube.js
@@ -1,40 +1,70 @@
 import React from "react";
-import Header from "../layouts/Header";
-import Footer from "../layouts/Footer";
+import { useEffect, useState } from "react";
+import YoutubeList from '../info/YoutubeList';
+import YoutubeSearch from '../info/YoutubeSearch';
 import Contents from "../layouts/Contents";
+import Footer from "../layouts/Footer";
+import Header from "../layouts/Header";
+import WrapTitle from "../layouts/WrapTitle";
+import ContInfo from "../layouts/ContInfo";
 
 function Youtube(){
+    const [videos, setVideos] = useState([]);
+
+    const start = () => {
+        setTimeout(()=>{
+            alert("ddd");
+        }, 2000);
+    }
+
+    const search = (query) => {
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+          };
+          
+          fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyBgxJkkTLxZdkGgn13bbuN_YzDxDyLCHj4&maxResults=30&type=video`, requestOptions)
+            .then(response => response.json())
+            .then(result => setVideos(result.items))
+            .catch(error => console.log('error', error));
+    }
+
+    useEffect( () => {
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+          };
+          
+          fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=haha ha&key=AIzaSyBgxJkkTLxZdkGgn13bbuN_YzDxDyLCHj4&maxResults=30&type=video", requestOptions)
+            .then(response => response.json())
+            // .then(result => console.log(result.items))
+            .then(result => {
+                setVideos(result.items);
+                start();
+            })
+            .catch(error => console.log('error', error));
+    }, []);
+
     return (
         <div>
             <Header />
             <Contents>
                 <section id="youtubeCont">
                     <div className="container">
-                        <div className="wrap__title">
-                            <h2><strong>Youtube</strong><em>Reference</em></h2>
-                        </div>
+                        <WrapTitle text={["youtube", "reference"]} />
                         <div className="youtube__cont">
+                            <YoutubeSearch onSearch={search} />
                             <div className="youtube">
-                                
+                                <YoutubeList videos={videos} />
                             </div>
                         </div>
                     </div>
                 </section>
-
-                <section id="contactInfo">
-                    <div className="contact__info">
-                        <div>
-                            <p>Let’s create something new</p>
-                            <h3>LET`S Get IN<br />TOUCH</h3>
-                            <a href="contact.html" className="contact">Contact Me</a>
-                        </div>
-                    </div>
-                </section>
+                <ContInfo />
             </Contents>
-
             <Footer />
         </div>
     )
-}
+ }
 
 export default Youtube;
